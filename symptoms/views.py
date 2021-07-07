7b9a52bebc413ae69408b69c16e818f94bd31516
Ammar <ammarosama011@gmail.com>
2021-07-07 22:21:23 +0200

    symptoms view added

--- symptoms/views.py
+++ symptoms/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 # Create your views here.
+
+from .models import Symptom 
+from .forms import SymptomForm
+from case.models import Case
+# Create your views here.
+
+def Symname(request):
+   # case = get_object_or_404 (Case,symptom_name)
+   
+    new_symptom = None
+
+    if request.method == 'POST' :
+        
+        
+        #symptom was posted
+        symptom_form = SymptomForm(data=request.POST)
+        if symptom_form.is_valid() :
+            #create symptom object but dont save to database
+            new_symptom = symptom_form.save(commit = False)
+            #Assign the current case to the symptom
+            #new_symptom.case = case
+            #Save the symptom to the database
+            new_symptom.save()
+    else :
+        symptom_form = SymptomForm()
+    
+    return render(request,
+                  'patient_symptoms.html',
+                  {#'case':case,
+                #    'symptoms':symptoms ,
+                   'new_symptom': new_symptom ,
+                   'symptom_form' : symptom_form})
