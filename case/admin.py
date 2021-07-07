8508dc5de9ba3e36f79491ec8c208460ed1bbf33
HMMK99 <hatemkamal1999@gmail.com>
2021-07-07 10:44:49 +0200

    completed admin

--- case/admin.py
+++ case/admin.py
@@ -1,72 +1,72 @@
 from django.contrib import admin
-from .models import Case, CaseSymptoms
+from .models import Case
 from desease.models import Desease
 import operator
 
-@admin.register(CaseSymptoms)
-class CaseSymptomAdmin(admin.ModelAdmin):
-    pass
+def get_desease(sympotoms):
+    '''
+    get the input symptoms
+    filter it and get the desease attached and count the desease
+    get the most frequent one and put ut in desease_name field
+    '''
+    # we only have the case units
+    # we extract its symptoms objects
+    # # sympotoms = []
+    # # for unit_case in unit_cases:
+    # #     sympotoms.append(unit_case.symptom_name)
+    
+    # then we can get the deseases connected to the same symp
+    # also create dict to have the count of the desease ocourence
+    desease = {}
+    for symptom in sympotoms:
+        ds = Desease.objects.filter(symptom_name=symptom)
+        for d in ds:
+            try:
+                desease[str(d.desease_name)] += 1
+            except:
+                desease[str(d.desease_name)] = 1
+    
+    sorted_d = dict(sorted(desease.items(), key=operator.itemgetter(1), reverse=True))
+    
+    max = 0
+    i = 0
+    for v in sorted_d.values():
+        if v >= max:
+            max = v
+            i += 1
+            continue
+        break
+    most_freq_desease = ' or '.join(list(sorted_d)[0:i])
+
+    return most_freq_desease
+
+# @admin.register(CaseSymptoms)
+# class CaseSymptomAdmin(admin.ModelAdmin):
+#     def response_add(self, request, obj, post_url_continue=None):
+#         obj.case.desease_name = self.get_desease(list(CaseSymptoms.objects.filter(case=obj.case)).append(obj.symptom_name))
+#         breakpoint()
+#         return super().response_add(request, obj, post_url_continue=post_url_continue)
 
 
-class InlineCaseSymptoms(admin.TabularInline):
-    model = CaseSymptoms
-    extra = 2
+
+# class InlineCaseSymptoms(admin.TabularInline):
+#     model = CaseSymptoms
+#     extra = 2
+
+
 
 @admin.register(Case)
 class CaseAdmin(admin.ModelAdmin):
     model = Case
-    inlines = (InlineCaseSymptoms, )
+    # inlines = (InlineCaseSymptoms, )
     list_display = ("id", "patient_name", "desease_name")
     list_display_links = ("id", "patient_name")
     list_filter = ("patient_name",)
     search_fields = ("patient_name",)
     ordering = ("-id",)
 
-    def get_desease(self, unit_cases):
-        '''
-        get the input symptoms
-        filter it and get the desease attached and count the desease
-        get the most frequent one and put ut in desease_name field
-        '''
-        # we only have the case units
-        # we extract its symptoms objects
-        sympotoms = []
-        for unit_case in unit_cases:
-            sympotoms.append(unit_case.symptom_name)
-        
-        # then we can get the deseases connected to the same symp
-        # also create dict to have the count of the desease ocourence
-        desease = {}
-        for symptom in sympotoms:
-            ds = Desease.objects.filter(symptom_name=symptom)
-            for d in ds:
-                try:
-                    desease[str(d.desease_name)] += 1
-                except:
-                    desease[str(d.desease_name)] = 1
-        
-        sorted_d = dict(sorted(desease.items(), key=operator.itemgetter(1), reverse=True))
-        
-        max = 0
-        i = 0
-        for v in sorted_d.values():
-            if v >= max:
-                max = v
-                i += 1
-                continue
-            break
-        most_freq_desease = ' or '.join(list(sorted_d)[0:i])
-        # if len(most_freq_desease) > 1:
-        #     most_freq_desease = most_freq_desease
-        # breakpoint()
-        # desease_obj = Desease.objects.get(desease_name=most_freq_desease)
-
-        return most_freq_desease
-
-    # def save_model(self, request, obj, form, change):
-    #     obj.desease_name = self.get_desease(list(CaseSymptoms.objects.filter(case=obj)))
-    #     super().save_model(request, obj, form, change)
 
     def response_add(self, request, obj, post_url_continue=None):
-        obj.desease_name = self.get_desease(list(CaseSymptoms.objects.filter(case=obj)))
-        return super().response_add(request, obj, post_url_continue=post_url_continue)
+        obj.desease_name = get_desease(list(obj.symptom_name.all()))
+        obj.save()
+        return super().response_add(request, obj, post_url_continue=post_url_continue)    
--- case/models.py
+++ case/models.py
@@ -7,15 +7,18 @@ class Case(models.Model):
         "user.User", on_delete=models.SET_NULL, null=True, blank=True
     )
     desease_name = models.CharField(max_length=255, blank=True)
+    symptom_name = models.ManyToManyField(
+        "symptoms.Symptom"
+    )
 
     def __str__(self):
         return str(self.patient_name) + '-' + self.desease_name
 
 
-class CaseSymptoms(models.Model):
-    case = models.ForeignKey(
-        "case.Case", on_delete=models.CASCADE, null=True, blank=True
-    )
-    symptom_name = models.ForeignKey(
-        "symptoms.Symptom", on_delete=models.SET_NULL, null=True, blank=True
-    )
+# class CaseSymptoms(models.Model):
+#     case = models.ForeignKey(
+#         "case.Case", on_delete=models.CASCADE, null=True, blank=True
+#     )
+#     symptom_name = models.ForeignKey(
+#         "symptoms.Symptom", on_delete=models.SET_NULL, null=True, blank=True
+#     )
